b8dcf028c51fa0b25822bb926249de7ce876eed9
성민석 <70127676+SungMinseok@users.noreply.github.com>
2023-04-21 19:30:23 +0900

    aa6

--- 놀이터/setvideo.py
+++ 놀이터/setvideo.py
@@ -1,4 +1,5 @@
 from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.editor import VideoFileClip
 
 def compress_video(input_file, output_file, start_time=None, end_time=None, bitrate="1500k"):
     """
@@ -13,12 +14,18 @@ def compress_video(input_file, output_file, start_time=None, end_time=None, bitr
     video_clip = VideoFileClip(input_file)
     video_clip = video_clip.without_audio()
 
+    if end_time == None :
+        duration = video_clip.duration
+    else : 
+        duration = end_time
+
     # Extract the specified segment if start_time and end_time are provided
     if start_time is not None and end_time is not None:
-        video_clip = video_clip.subclip(start_time, end_time)
+        video_clip = video_clip.subclip(start_time, duration)
 
     # Compress the video
     video_clip.write_videofile(output_file, bitrate=bitrate)
 
 
-compress_video(f'video50.mp4', "compressed_video.mp4", start_time=0, end_time=35, bitrate="1500k")
+#compress_video(f'video53.mp4', "compressed_video.mp4", start_time=16, end_time=duration, bitrate="1500k")
+#compress_video(f'video56.mp4', "compressed_video.mp4", start_time=0, end_time=12, bitrate="1500k")
